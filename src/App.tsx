732b4d70fcae6f87d76afc8f7ae30de2499167f7
lakardion <lakardion@protonmail.com>
2022-08-07 17:02:29 -0300

    feat(switchfree) add switch component with sizing

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,36 @@ const Switch: FC<{ onChange?: (value: boolean) => void }> = ({ onChange }) => {
 }
 
 
+const SwitchFree: FC<{ onChange?: (value: boolean) => void, size: number }> = ({ onChange, size }) => {
+  const [value, setValue] = useState(false)
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value === 'true'
+    setValue(newValue)
+    onChange?.(newValue)
+  }
+  const toggleChange = () => {
+    setValue(prevValue => !prevValue)
+  }
+
+  const translateClass = value ? `translate-x-full` : ''
+  const bgSwitch = value ? `bg-green-500` : ''
+  const ratio = 1.2
+  return <section style={{ width: `${size * 2}px` }} className={`aspect-video`}>
+    <input hidden type="checkbox" checked={value} value={value.toString()} onChange={handleChange} />
+    <div className={`w-full h-full bg-gray-400 rounded-l-full rounded-r-full relative hover:cursor-pointer ${bgSwitch}`} onClick={toggleChange}>
+      <div style={{ left: `${size / (ratio * 5)}px`, width: `${size / ratio}px`, height: `${size / ratio}px` }} className={`absolute bg-gray-100 rounded-full transition-transform ease-linear duration-200  transform hover:scale-105 hover:bg-teal-100 -translate-y-1/2 top-1/2 ${translateClass}`}>
+
+      </div>
+    </div>
+  </section>
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="flex justify-center items-center h-screen w-screen">
-      <Switch />
+      <SwitchFree size={100} />
     </div>
   )
 }
