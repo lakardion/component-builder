cbc6c4ea30278103e5fb7825e2f4714145334770
lakardion <lakardion@protonmail.com>
2023-01-03 07:59:33 -0300

    refactor(progress-bar) add c.hook clean up

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -1,13 +1,32 @@
 import {
+  FC,
+  MouseEvent,
+  MutableRefObject,
+  useEffect,
   useRef,
   useState,
-  useEffect,
-  FC,
-  DragEvent,
-  MouseEvent,
-  useMemo,
 } from "react";
 import { secondsToDuration } from "../utils/date";
+
+const useWatchWidth = (ref: MutableRefObject<HTMLDivElement | null>) => {
+  const [width, setWidth] = useState<number>();
+  useEffect(() => {
+    if (!width && ref.current) {
+      setWidth(ref.current.clientWidth);
+    }
+    const domListener: ResizeObserverCallback = (e) => {
+      const [first] = e;
+      setWidth(first.target.clientWidth);
+    };
+    const obs = new ResizeObserver(domListener);
+    ref.current && obs.observe(ref.current);
+    return () => {
+      obs.disconnect();
+    };
+  }, [width]);
+
+  return width;
+};
 
 export const ProgressBar: FC<{
   totalSeconds: number;
@@ -16,34 +35,17 @@ export const ProgressBar: FC<{
   onChange: (newSeconds: number) => void;
 }> = ({ currentSeconds, totalSeconds, className = "", onChange }) => {
   const progressBarRef = useRef<HTMLDivElement | null>(null);
-  const [currentProgressBarWidth, setCurrentProgressBarWidth] =
-    useState<number>();
-  const currentProgressDotRef = useRef<HTMLDivElement | null>(null);
+  const currentProgressBarWidth = useWatchWidth(progressBarRef);
 
-  useEffect(() => {
-    if (!currentProgressBarWidth && progressBarRef.current) {
-      setCurrentProgressBarWidth(progressBarRef.current.clientWidth);
-    }
-    const domListener: ResizeObserverCallback = (e) => {
-      const [first] = e;
-      setCurrentProgressBarWidth(first.target.clientWidth);
-    };
-    const obs = new ResizeObserver(domListener);
-    progressBarRef.current && obs.observe(progressBarRef.current);
-    return () => {
-      obs.disconnect();
-    };
-  }, [currentProgressBarWidth]);
-
-  // const [isDraggingProgress, setIsDraggingProgress] = useState(false);
   const isDraggingProgressRef = useRef(false);
   const [
     /**
-     * Intermediary seconds to instantly show seconds on progress bar, but only commit them once the use stops dragging
+     * Intermediary seconds to instantly show seconds on progress bar, but only commit them once the user lets go of the mouse (onMouseUp)
      */
     instantSeconds,
     setInstantSeconds,
   ] = useState(currentSeconds);
+
   const onMouseDown = (e: MouseEvent) => {
     e.stopPropagation();
     isDraggingProgressRef.current = true;
@@ -80,6 +82,14 @@ export const ProgressBar: FC<{
     setInstantSeconds(newSeconds);
     onChange(newSeconds);
   };
+
+  // sync with external currentSeconds
+  useEffect(() => {
+    setInstantSeconds((is) => {
+      if (is !== currentSeconds) return currentSeconds;
+      return is;
+    });
+  }, [currentSeconds]);
 
   return (
     <section
@@ -119,7 +129,6 @@ export const ProgressBar: FC<{
         <div
           className="h-3.5 w-3.5 cursor-pointer rounded-full bg-black dark:bg-white"
           onMouseDown={onMouseDown}
-          ref={currentProgressDotRef}
           style={{
             transform: `translateX(${
               currentProgressBarWidth && totalSeconds
